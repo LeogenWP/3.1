27de4a6b3dbef4ee965fab1ff924ec623554b615
LeogenWP <gpankratov95@gmail.com>
2023-04-04 16:10:56 +0300

    fix saving process of companies

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,8 +2,6 @@ package com.example.demo;
 
 import com.example.demo.util.Starter;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/com/example/demo/service/DB/CompanyInfoUpdater.java
+++ src/main/java/com/example/demo/service/DB/CompanyInfoUpdater.java
@@ -3,47 +3,51 @@ package com.example.demo.service.DB;
 import com.example.demo.dao.CompanyRepository;
 import com.example.demo.model.Company;
 import com.example.demo.util.Executor;
-import com.example.demo.util.cache.CompanyManager;
 import com.example.demo.util.cache.QueueManager;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import java.util.concurrent.ExecutionException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.concurrent.Future;
 
 @Component
 @RequiredArgsConstructor
 public class CompanyInfoUpdater {
-    private final CompanyRepository repo;
 
+    private final CompanyRepository repo;
     private final Executor executor;
+
+    @Value("${executor.treads-amount}")
+    private int treadNums;
 
     public void startWork()  {
 
+        List<Future<Company>> compamiesFuture = Collections.synchronizedList(new ArrayList<>());
+
         while (QueueManager.getQueue().size() > 0) {
-            for (int i = 0; i < 6; i++) {
-                try {
-                    CompanyCallable company = new CompanyCallable();
-                    Future<Company> companyFuture = executor.getExecutor().submit(company);
-                    if (companyFuture.get() != null) {
-                        saveCompany(companyFuture.get());
-                    }
-                } catch (InterruptedException e) {
-                    System.out.println("!!!!! " + e);
-                } catch (ExecutionException e) {
-                    System.out.println("!!!!! " + e);
-                }
+            for (int i = 0; i < treadNums; i++) {
+                CompanyCallable company = new CompanyCallable();
+                compamiesFuture.add(executor.getExecutor().submit(company));
             }
         }
-
+        saveCompanies(compamiesFuture);
     }
 
-    private void saveCompany(Company company) {
-        Company cashedCompany = CompanyManager.getCompanies().get(company.getSymbol());
-        if (cashedCompany == null || !cashedCompany.equals(company)) {
-            repo.save(company);
-            CompanyManager.getCompanies().put(company.getSymbol(), company);
+    private void saveCompanies(List<Future<Company>> compamiesFuture) {
+        List<Company> companies = new ArrayList<>();
+        for (Future<Company> future : compamiesFuture) {
+            try {
+                if (future.get() != null) {
+                    companies.add(future.get());
+                }
+            } catch (Exception e) {
+                System.out.println("Smth bad happens in CompanyInfoUpdater.saveCompanies: " + e);
+            }
         }
+        repo.saveAll(companies);
     }
 
 }
